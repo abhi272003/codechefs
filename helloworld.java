0a3cf7f1f27e86f87c994536529d8ead79acc388
abhi <136185482+abhi272003@users.noreply.github.com>
2023-09-06 19:19:56 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Codechef {
+  public static void main(String[] args) {
+    System.out.print("Hello");
+    System.out.println();
+    System.out.print("World");
+  }
+}
